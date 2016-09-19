1a9e6e956378a21ec365eed91a56af9393d2d723
Andri Yadi <andri@dycode.com>
2016-09-19 00:00:28 +0000

    Move start script

---
+++ start-term.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export PATH=/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin
+sudo /home/pi/lora/PatientCare-Gateway/LoRaGateway/lora_gateway | sudo node /home/pi/lora/PatientCare-Gateway/LoraProcessing/index.js
